0e9f0f2f048cf09fef53071f3e95dc86af274803
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2024-01-08 20:09:04 +0200

    create models and views and templets

--- post/models.py
+++ post/models.py
@@ -2,7 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+CATEGORY_CHOICES =(
+    ("WB","Web Development"),
+    ("DS", "Data Science"),
+)
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField()
+    published_at = models.BooleanField(default=True)
+    email = models.EmailField( max_length=254, null=True , blank=True)
+    views_count= models.IntegerField(default=0)
+    category = models.CharField(choices= CATEGORY_CHOICES, max_length=20)
+    
+    
+    
+    def __str__(self):
+        return self.title
