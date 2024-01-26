a9fc48d0129fc1b8d89c997edd41343c98464630
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2024-01-27 01:40:51 +0200

    create profile part two

--- project/urls.py
+++ project/urls.py
@@ -20,8 +20,10 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('accounts/', include('accounts.urls' , namespace='accounts')),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
-    path('blog/', include('post.urls' , namespace='blog'))
+    path('blog/', include('post.urls' , namespace='blog')),
 
 ]
 
